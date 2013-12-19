38d3244d0fe2ea7ee216fbec94c4a4c52c076759
Eric Streske <estreske@gmail.com>
2013-12-19 14:43:40 -0500

    added configs for different environments

---
+++ config/environments.rb
@@ -0,0 +1,22 @@
+configure :development do 
+  ActiveRecord::Base.establish_connection(
+      :adapter => "postgresql",
+      :host => "localhost",
+      :username => "estreske",
+      :database => "simple_blog2",
+      :encoding => "utf8"
+    )
+end
+
+configure :production do 
+  db = URI.parse(ENV['HEROKU_POSTGRESQL_OLIVE_URL'])
+  #configuration info
+  ActiveRecord::Base.establish_connection(
+      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host    => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+  )
+end
