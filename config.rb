0021a25ea747a66f133a612cec8784374a36c4dd
Eric Streske <estreske@gmail.com>
2013-12-20 16:02:54 -0500

    fixed config for Heroku

--- config.rb
+++ config.rb
@@ -1,8 +1,9 @@
 # establishes our connection to the DB and other settings
 ActiveRecord::Base.establish_connection(
     :adapter => "postgresql",
-    :host => "localhost",
-    :username => "omardelarosa",
-    :database => "simple_blog",
+    :host => "ec2-54-204-42-135.compute-1.amazonaws.com",
+    :username => "myxfhsihioudbl",
+    :password => "y86q1QCZEbtRJH8b6msQRFoRlE"
+    :database => "dejcpqml978ivp",
     :encoding => "utf8"
   )
